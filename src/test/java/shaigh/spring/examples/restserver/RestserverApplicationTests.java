e0147cad26b18324b7f6f3ffbd61ab2d04fa01ee
Steve Haigh <steve.haigh@skyscanner.net>
2018-02-02 13:00:42 +0000

    Really created initial boilerplate using Initializr.

---
+++ src/test/java/shaigh/spring/examples/restserver/RestserverApplicationTests.java
@@ -0,0 +1,16 @@
+package shaigh.spring.examples.restserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
